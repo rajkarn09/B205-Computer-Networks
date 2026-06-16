0f0f07d7b2083baf143087c4aa5259253dc12340
Rajkaran singh <rajkaran092004@gmail.com>
2026-06-16 18:19:36 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -24,3 +24,60 @@ def remove_client(client):
         del usernames[client]
         client.close()
 
+# handling client
+def handle_client(client):
+    try:
+        # handling authentication
+        client.sendall(b"USERNAME")
+        username = client.recv(1024).decode()
+        usernames[client] = username
+        clients.append(client)
+
+        print(f"connecting {username}")
+        broadcast(f"{username} joined\n".encode(), client)
+
+        while True:
+            data = client.recv(1024)
+
+            if not data:
+                break
+
+            # handling file transfer
+            if data.startswith(b"FILE|"):
+                broadcast(data, client)
+
+                size = int(client.recv(1024).decode())
+                broadcast(str(size).encode(), client)
+
+                received = 0
+                while received < size:
+                    chunk = client.recv(min(1024, size - received))
+                    if not chunk:
+                        break
+                    received += len(chunk)
+                    broadcast(chunk, client)
+
+            else:
+                msg = f"{username}: {data.decode()}\n"
+                print(msg.strip())
+                broadcast(msg.encode(), client)
+
+    except:
+        pass
+
+    remove_client(client)
+
+# starting server
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen(5)
+
+    print("listening...")
+
+    while True:
+        client, _ = server.accept()
+        threading.Thread(target=handle_client, args=(client,)).start()
+
+if __name__ == "__main__":
+    start_server()
